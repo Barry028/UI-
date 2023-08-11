a23acec7e1661e1ae046bc0f3414e7397b814e64
Barry Lin <39817188+Barry028@users.noreply.github.com>
2023-08-12 05:12:56 +0800

    0812

--- node.js
+++ node.js
@@ -2,8 +2,8 @@ const gradientBadge = require('gradient-badge');
 exports.endpoint = function (req, resp) {
     resp.setHeader('Content-Type', 'image/svg+xml');
     resp.end(gradientBadge({
-        subject: 'subject',
-        status: 'status',
+        subject: '',
+        status: 'statusasdasdas',
         style: 'flat',
         gradient: ['ff00fb', '00faf2', '4400ff']
     }));
